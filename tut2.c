40587071192a16fe4430ea4e1a8ebdc65100e0a9
UzitheI <133661992+UzitheI@users.noreply.github.com>
2023-05-15 18:29:49 +0545

    Add files via upload
    tut2trial.c

---
+++ tut2.c
@@ -0,0 +1,19 @@
+/*int stores int value
+float stores decimal
+char stores character  values*/
+#include<stdio.h>
+int main(){
+    int age=22;
+    float pi=3.14;
+    char d='e';
+    //constants store values which cant be changed there are 3 types of constants
+    //they are integer const and real const and finally character
+    /*integer const stores negative and postive integers, real stores values like 1.0 and 2.0,-2.4, char constat makes it easy to store character values*/
+    const int e=34;
+    /*keywords in C
+    these keywords are reserved in C, which makes it easy for it to be used but we cannot use them to determine any other value
+    int is a type of value*/
+    return 0;
+
+
+}
